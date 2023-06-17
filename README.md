621721d8b0d6a4c25a987e9a53a90d90064586a2
Pasha Piankovskyi <ghostangel21042017@gmail.com>
2023-06-17 17:55:03 +0300

    add task

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# goit-nodeJs-hw-1
+Посилання на скріншоти:
+Remove: "https://monosnap.com/file/4HfAJC4BQDS7jdlDMLkroMfdsR1IqV";
+List: "https://monosnap.com/file/8FtHk9MM4gIexdP4LjMBez5KSlgFb4";
+Get: "https://monosnap.com/file/ldbqDB8DBaMr6B7IZTVXhCxKx3UKcZ";
+Add: "https://monosnap.com/file/wvHS7gczk9UXGxjxgXVh0cepIK4wEZ";
---
+++ index.js
@@ -0,0 +1,35 @@
+const { Command } = require("commander");
+const contacts = require("./contacts");
+const program = new Command();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-e, --email <type>")
+  .option("-p, --phone <type>");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+invokeAction(argv);
